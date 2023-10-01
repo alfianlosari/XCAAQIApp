05b6e1eec9103c46f3aac15ae2b6c930fda271ff
Alfian Losari <alfianlosari@gmail.com>
2023-10-01 10:43:58 +0700

    Initial Commit

---
+++ XCAAQIndex/XCAAQIndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAAQIndexApp.swift
+//  XCAAQIndex
+//
+//  Created by Alfian Losari on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct XCAAQIndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
